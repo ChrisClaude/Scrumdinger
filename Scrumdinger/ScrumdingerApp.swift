5e2a1212741d378ab6c18014a9e51ca934828955
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-10-28 14:17:36 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Claude De-Tchambila on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
