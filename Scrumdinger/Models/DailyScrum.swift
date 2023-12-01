69fa160d9a842c82e6477af493b1e2bfc07b1a5d
Chris Claude <christ.tchambila@gmail.com>
2023-12-01 08:26:38 +0100

    Managing state and lifecycle section 7

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -52,15 +52,15 @@ extension DailyScrum {
     [
         DailyScrum(title: "Design",
                    attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
-                   lengthInMinutes: 10,
+                   lengthInMinutes: 1,
                    theme: .yellow),
         DailyScrum(title: "App Dev",
                    attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
-                   lengthInMinutes: 5,
+                   lengthInMinutes: 2,
                    theme: .orange),
         DailyScrum(title: "Web Dev",
                    attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
-                   lengthInMinutes: 5,
+                   lengthInMinutes: 3,
                    theme: .poppy)
     ]
 }
